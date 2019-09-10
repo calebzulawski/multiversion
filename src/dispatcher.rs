640d95fc5ae4200abca2a868396bffd7d6cfe488
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-09-09 20:10:23 -0400

    Add documentation, fix bug regarding mixing unsafe fn and fn

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -11,35 +11,7 @@ pub(crate) struct Dispatcher {
 
 impl ToTokens for Dispatcher {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let feature_detection = {
-            let defaulted_arches = self.functions.iter().filter_map(|(target, _)| {
-                if !target.has_features_specified() {
-                    Some(target.arch_as_str())
-                } else {
-                    None
-                }
-            });
-            let return_if_detected = self.functions.iter().map(|(target, function)| {
-                let target_arch = target.target_arch();
-                let features_detected = target.features_detected();
-                quote! {
-                    #target_arch
-                    {
-                        if #features_detected {
-                            return #function
-                        }
-                    }
-                }
-            });
-            let default = &self.default;
-            quote! {
-                #(#return_if_detected)*
-                #[cfg(not(any(#(target_arch = #defaulted_arches),*)))]
-                {
-                    return #default
-                }
-            }
-        };
+        let function_arguments = &self.signature.inputs.iter();
         let argument_names = &self
             .signature
             .inputs
@@ -65,9 +37,49 @@ impl ToTokens for Dispatcher {
             })
             .collect::<Vec<_>>();
         let returns = &self.signature.output;
+        let feature_detection = {
+            let defaulted_arches = self.functions.iter().filter_map(|(target, _)| {
+                if !target.has_features_specified() {
+                    Some(target.arch_as_str())
+                } else {
+                    None
+                }
+            });
+            let return_if_detected = self.functions.iter().map(|(target, function)| {
+                let target_arch = target.target_arch();
+                let features_detected = target.features_detected();
+                let function_arguments = function_arguments.clone();
+                let argument_names = argument_names.clone();
+                quote! {
+                    #target_arch
+                    {
+                        // wrap the function in an unsafe function with the same signature
+                        unsafe fn unsafe_wrapper(#(#function_arguments),*) #returns {
+                            #function(#(#argument_names),*)
+                        }
+                        if #features_detected {
+                            return unsafe_wrapper
+                        }
+                    }
+                }
+            });
+            let default = &self.default;
+            let function_arguments = function_arguments.clone();
+            let argument_names = argument_names.clone();
+            quote! {
+                #(#return_if_detected)*
+                #[cfg(not(any(#(target_arch = #defaulted_arches),*)))]
+                {
+                    unsafe fn unsafe_wrapper(#(#function_arguments),*) #returns {
+                        #default(#(#argument_names),*)
+                    }
+                    return unsafe_wrapper
+                }
+            }
+        };
         tokens.extend(quote! {
             use std::sync::atomic::{AtomicUsize, Ordering};
-            type __fn_ty = fn (#(#argument_ty),*) #returns;
+            type __fn_ty = unsafe fn (#(#argument_ty),*) #returns;
             fn __get_fn() -> __fn_ty {
                 #feature_detection
             }
@@ -77,8 +89,10 @@ impl ToTokens for Dispatcher {
                 __current_ptr = unsafe { std::mem::transmute(__get_fn()) };
                 __DISPATCHED_FN.store(__current_ptr, Ordering::SeqCst);
             }
-            let __current_fn = unsafe { std::mem::transmute::<usize, __fn_ty>(__current_ptr) };
-            __current_fn(#(#argument_names),*)
+            unsafe {
+                let __current_fn = std::mem::transmute::<usize, __fn_ty>(__current_ptr);
+                __current_fn(#(#argument_names),*)
+            }
         });
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,76 @@
+//! This crate provides macro the [`target_clones`] attribute and [`multiversion!`] macro for
+//! implementing function multiversioning.
+//!
+//! [`target_clones`]: attr.target_clones.html
+//! [`multiversion!`]: macro.multiversion.html
+//!
+//! Many CPU architectures have a variety of instruction set extensions that provide additional
+//! functionality. Common examples are single instruction, multiple data (SIMD) extensions such as
+//! SSE and AVX on x86/x86-64 and NEON on ARM/AArch64. When available, these extended features can
+//! provide significant speed improvements to some functions. These optional features cannot be
+//! haphazardly compiled into programs–executing an unsupported instruction will result in a
+//! crash.  Function multiversioning is the practice of compiling multiple versions of a function
+//! with various features enabled and detecting which version to use at runtime.
+//!
+//! # Example
+//! The following example is a good candidate for optimization with SIMD.  The function `square`
+//! optionally uses the AVX instruction set extension on x86 or x86-64.  The SSE instruciton set
+//! extension is part of x86-64, but is optional on x86 so the square function optionally detects
+//! that as well.
+//!
+//! This is works by compiling multiple *clones* of the function with various features enabled and
+//! detecting which to use at runtime. If none of the targets match the current CPU (e.g. an older
+//! x86-64 CPU, or another architecture such as ARM), a clone without any features enabled is used.
+//! ```
+//! use multiversion::target_clones;
+//!
+//! #[target_clones("[x86|x86_64]+avx", "x86+sse")]
+//! fn square(x: &mut [f32]) {
+//!     for v in x {
+//!         *v *= *v;
+//!     }
+//! }
+//! ```
+//!
+//! The following produces a nearly identical function, but instead of cloning the function, the
+//! implementations are manually specified.  This is typically more useful when the implementations
+//! aren't identical, such as when using explicit SIMD instructions instead of relying on compiler
+//! optimizations.
+//! ```
+//! use multiversion::multiversion;
+//!
+//! multiversion!{
+//!     fn square(x: &mut [f32])
+//!     "[x86|x86_64]+avx" => square_avx,
+//!     "x86+sse" => square_sse,
+//!     default => square_generic,
+//! }
+//!
+//! #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+//! #[target_feature(enable = "avx")]
+//! unsafe fn square_avx(x: &mut [f32]) {
+//!     for v in x {
+//!         *v *= *v;
+//!     }
+//! }
+//!
+//! #[cfg(target_arch = "x86")]
+//! #[target_feature(enable = "sse")]
+//! unsafe fn square_avx(x: &mut [f32]) {
+//!     for v in x {
+//!         *v *= *v;
+//!     }
+//! }
+//!
+//! fn square_generic(x: &mut [f32]) {
+//!     for v in x {
+//!         *v *= *v;
+//!     }
+//! }
+//!
+//! # fn main() {}
+//! ```
+
 extern crate proc_macro;
 
 mod dispatcher;
