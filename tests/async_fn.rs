542ea13a41fa37413a834118a8723c1630bfb0e5
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-11-10 19:28:20 -0500

    Disable async test on older compilers

--- tests/async_fn.rs
+++ tests/async_fn.rs
@@ -1,19 +1,16 @@
-use multiversion::target_clones;
-use futures::executor::block_on;
-
-#[target_clones("[x86|x86_64]+avx", "[x86|x86_64]+sse", "[arm|aarch64]+neon")]
+#[rustversion::since(1.39)]
+#[multiversion::target_clones("[x86|x86_64]+avx", "[x86|x86_64]+sse", "[arm|aarch64]+neon")]
 async fn async_add(a: &mut [f32], b: &[f32]) {
     a.iter_mut().zip(b.iter()).for_each(|(a, b)| *a += b);
 }
 
 mod test {
-    use super::*;
-
+    #[rustversion::since(1.39)]
     #[test]
     fn async_fn() {
         let mut a = vec![0f32, 2f32, 4f32];
         let b = vec![1f32, 1f32, 1f32];
-        block_on(async_add(&mut a, &b));
+        futures::executor::block_on(super::async_add(&mut a, &b));
         assert_eq!(a, vec![1f32, 3f32, 5f32]);
     }
 }
