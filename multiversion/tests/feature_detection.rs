57527fd3dea3fe6a1273e16117527bb55e5e1dc3
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-07-24 22:27:51 -0400

    Remove feature detection macro

--- multiversion/tests/feature_detection.rs
+++
@@ -1,7 +0,0 @@
-use multiversion::are_cpu_features_detected;
-
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[test]
-fn detect_x86() {
-    let _: bool = are_cpu_features_detected!("sse", "avx");
-}
