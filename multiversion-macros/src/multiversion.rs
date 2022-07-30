ee4b90361d7d790c3bb1f8a1c0ea0383909952d5
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-07-29 23:18:50 -0400

    Simply multiversion macro parsing

--- multiversion-macros/src/multiversion.rs
+++ multiversion-macros/src/multiversion.rs
@@ -2,13 +2,10 @@ use crate::dispatcher::{DispatchMethod, Dispatcher};
 use crate::target::Target;
 use proc_macro2::{Span, TokenStream};
 use quote::ToTokens;
-use std::{
-    collections::HashMap,
-    convert::{TryFrom, TryInto},
-};
+use std::{collections::HashMap, convert::TryFrom};
 use syn::{
-    parse::Parser, parse_quote, punctuated::Punctuated, spanned::Spanned, token::Comma, Attribute,
-    Error, ItemFn, Lit, LitStr, Meta, NestedMeta, Path, ReturnType, Type,
+    parse::Parser, parse_quote, punctuated::Punctuated, spanned::Spanned, token::Comma, Error,
+    ItemFn, Lit, LitStr, Meta, NestedMeta, ReturnType, Type,
 };
 
 fn meta_path_string(meta: &Meta) -> Result<String, Error> {
@@ -80,109 +77,6 @@ impl MetaMap {
     }
 }
 
-struct Function {
-    targets: Vec<Target>,
-    inner_attrs: Vec<Attribute>,
-    func: ItemFn,
-    crate_path: Path,
-    dispatcher: DispatchMethod,
-}
-
-impl Function {
-    fn new(attr: Punctuated<NestedMeta, Comma>, func: ItemFn) -> Result<Self, Error> {
-        let mut map = MetaMap::try_from(attr)?;
-
-        let targets = if let Some(targets) = map.try_remove("targets") {
-            if let Meta::List(list) = targets {
-                list.nested
-                    .into_iter()
-                    .map(|x| {
-                        if let NestedMeta::Lit(lit) = &x {
-                            let target = Target::parse(lit_str(lit)?)?;
-                            if target.has_features_specified() {
-                                Ok(target)
-                            } else {
-                                Err(Error::new(x.span(), "target must have features specified"))
-                            }
-                        } else {
-                            Err(Error::new(x.span(), "expected target string"))
-                        }
-                    })
-                    .collect::<Result<Vec<_>, _>>()
-            } else {
-                Err(Error::new(
-                    targets.span(),
-                    "expected list of function clone targets",
-                ))
-            }
-        } else {
-            Err(Error::new(map.span(), "expected `targets`"))
-        }?;
-
-        let inner_attrs = if let Some(attrs) = map.try_remove("attrs") {
-            if let Meta::List(list) = attrs {
-                Ok(list
-                    .nested
-                    .into_iter()
-                    .map(|x| {
-                        parse_quote! { #[#x] }
-                    })
-                    .collect())
-            } else {
-                Err(Error::new(attrs.span(), "expected list of attributes"))
-            }
-        } else {
-            Ok(Vec::new())
-        }?;
-
-        let dispatcher = map
-            .try_remove("dispatcher")
-            .map(|x| {
-                let s = meta_kv_value(x)?;
-                match lit_str(&s)?.value().as_str() {
-                    "default" => Ok(DispatchMethod::Default),
-                    "static" => Ok(DispatchMethod::Static),
-                    "direct" => Ok(DispatchMethod::Direct),
-                    "indirect" => Ok(DispatchMethod::Indirect),
-                    _ => Err(Error::new(
-                        s.span(),
-                        "expected `default`, `static`, `direct`, or `indirect`",
-                    )),
-                }
-            })
-            .unwrap_or_else(|| Ok(DispatchMethod::Default))?;
-        let crate_path = map
-            .try_remove("crate_path")
-            .map(|x| lit_str(&meta_kv_value(x)?)?.parse())
-            .unwrap_or_else(|| Ok(parse_quote!(multiversion)))?;
-        map.finish()?;
-        Ok(Self {
-            targets,
-            inner_attrs,
-            crate_path,
-            dispatcher,
-            func,
-        })
-    }
-}
-
-impl TryFrom<Function> for Dispatcher {
-    type Error = Error;
-
-    fn try_from(item: Function) -> Result<Self, Self::Error> {
-        Ok(Self {
-            targets: item.targets,
-            block: *item.func.block,
-            inner_attrs: item.inner_attrs,
-            outer_attrs: item.func.attrs,
-            vis: item.func.vis,
-            sig: item.func.sig,
-            crate_path: item.crate_path,
-            dispatcher: item.dispatcher,
-        })
-    }
-}
-
 pub(crate) fn make_multiversioned_fn(
     attr: TokenStream,
     func: ItemFn,
@@ -197,8 +91,82 @@ pub(crate) fn make_multiversioned_fn(
     }
 
     let parser = Punctuated::parse_terminated;
-    let attr = parser.parse2(attr)?;
-    let function = Function::new(attr, func)?;
-    let dispatcher: Dispatcher = function.try_into()?;
-    Ok(dispatcher.to_token_stream())
+    let mut map = MetaMap::try_from(parser.parse2(attr)?)?;
+
+    let targets = if let Some(targets) = map.try_remove("targets") {
+        if let Meta::List(list) = targets {
+            list.nested
+                .into_iter()
+                .map(|x| {
+                    if let NestedMeta::Lit(lit) = &x {
+                        let target = Target::parse(lit_str(lit)?)?;
+                        if target.has_features_specified() {
+                            Ok(target)
+                        } else {
+                            Err(Error::new(x.span(), "target must have features specified"))
+                        }
+                    } else {
+                        Err(Error::new(x.span(), "expected target string"))
+                    }
+                })
+                .collect::<Result<Vec<_>, _>>()
+        } else {
+            Err(Error::new(
+                targets.span(),
+                "expected list of function clone targets",
+            ))
+        }
+    } else {
+        Err(Error::new(map.span(), "expected `targets`"))
+    }?;
+
+    let inner_attrs = if let Some(attrs) = map.try_remove("attrs") {
+        if let Meta::List(list) = attrs {
+            Ok(list
+                .nested
+                .into_iter()
+                .map(|x| {
+                    parse_quote! { #[#x] }
+                })
+                .collect())
+        } else {
+            Err(Error::new(attrs.span(), "expected list of attributes"))
+        }
+    } else {
+        Ok(Vec::new())
+    }?;
+
+    let dispatcher = map
+        .try_remove("dispatcher")
+        .map(|x| {
+            let s = meta_kv_value(x)?;
+            match lit_str(&s)?.value().as_str() {
+                "default" => Ok(DispatchMethod::Default),
+                "static" => Ok(DispatchMethod::Static),
+                "direct" => Ok(DispatchMethod::Direct),
+                "indirect" => Ok(DispatchMethod::Indirect),
+                _ => Err(Error::new(
+                    s.span(),
+                    "expected `default`, `static`, `direct`, or `indirect`",
+                )),
+            }
+        })
+        .unwrap_or_else(|| Ok(DispatchMethod::Default))?;
+    let crate_path = map
+        .try_remove("crate_path")
+        .map(|x| lit_str(&meta_kv_value(x)?)?.parse())
+        .unwrap_or_else(|| Ok(parse_quote!(multiversion)))?;
+    map.finish()?;
+
+    Ok(Dispatcher {
+        targets,
+        block: *func.block,
+        inner_attrs,
+        outer_attrs: func.attrs,
+        vis: func.vis,
+        sig: func.sig,
+        crate_path,
+        dispatcher,
+    }
+    .to_token_stream())
 }
