4e91cbebfd8b74932df0f9039f7476b42445861e
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-07-31 01:36:19 -0400

    Update benchmarks

--- multiversion-bench/benches/benchmarks/square.rs
+++ multiversion-bench/benches/benchmarks/square.rs
@@ -1,14 +1,7 @@
 use criterion::{criterion_group, Bencher, Criterion};
-use multiversion::multiversion;
+use multiversion_bench::*;
 use rand::distributions::Standard;
 use rand::Rng;
-
-#[multiversion(clones("[x86|x86_64]+avx"))]
-fn square(i: &[f32], o: &mut [f32]) {
-    for (i, o) in i.iter().zip(o) {
-        *o = i * i;
-    }
-}
 
 pub fn bench(c: &mut Criterion) {
     assert!(is_x86_feature_detected!("avx"));
@@ -19,18 +12,38 @@ pub fn bench(c: &mut Criterion) {
             .collect::<Vec<_>>();
         let mut group = c.benchmark_group(&format!("square {} values", i));
         group
-            .bench_with_input("generic", &input, |b: &mut Bencher, i: &Vec<f32>| {
-                let mut o = vec![0f32; i.len()];
-                b.iter(|| square_default_version(i.as_slice(), o.as_mut_slice()))
-            })
-            .bench_with_input("AVX (direct)", &input, |b: &mut Bencher, i: &Vec<f32>| {
-                let mut o = vec![0f32; i.len()];
-                b.iter(|| unsafe { square_avx_version(i.as_slice(), o.as_mut_slice()) })
-            })
-            .bench_with_input("AVX (via FMV)", &input, |b: &mut Bencher, i: &Vec<f32>| {
-                let mut o = vec![0f32; i.len()];
-                b.iter(|| square(i.as_slice(), o.as_mut_slice()))
-            });
+            .bench_with_input(
+                "default features",
+                &input,
+                |b: &mut Bencher, i: &Vec<f32>| {
+                    let mut o = vec![0f32; i.len()];
+                    b.iter(|| square(i.as_slice(), o.as_mut_slice()))
+                },
+            )
+            .bench_with_input(
+                "AVX (no multiversioning)",
+                &input,
+                |b: &mut Bencher, i: &Vec<f32>| {
+                    let mut o = vec![0f32; i.len()];
+                    b.iter(|| unsafe { square_avx(i.as_slice(), o.as_mut_slice()) })
+                },
+            )
+            .bench_with_input(
+                "AVX (indirect dispatch)",
+                &input,
+                |b: &mut Bencher, i: &Vec<f32>| {
+                    let mut o = vec![0f32; i.len()];
+                    b.iter(|| square_indirect(i.as_slice(), o.as_mut_slice()))
+                },
+            )
+            .bench_with_input(
+                "AVX (direct dispatch)",
+                &input,
+                |b: &mut Bencher, i: &Vec<f32>| {
+                    let mut o = vec![0f32; i.len()];
+                    b.iter(|| square_direct(i.as_slice(), o.as_mut_slice()))
+                },
+            );
         group.finish();
     }
 }
--- multiversion-bench/src/lib.rs
+++ multiversion-bench/src/lib.rs
@@ -1,1 +1,28 @@
+use multiversion::multiversion;
 
+pub fn square(i: &[f32], o: &mut [f32]) {
+    for (i, o) in i.iter().zip(o) {
+        *o = i * i;
+    }
+}
+
+#[target_feature(enable = "avx")]
+pub unsafe fn square_avx(i: &[f32], o: &mut [f32]) {
+    for (i, o) in i.iter().zip(o) {
+        *o = i * i;
+    }
+}
+
+#[multiversion(targets("x86_64+avx"), dispatcher = "indirect")]
+pub fn square_indirect(i: &[f32], o: &mut [f32]) {
+    for (i, o) in i.iter().zip(o) {
+        *o = i * i;
+    }
+}
+
+#[multiversion(targets("x86_64+avx"), dispatcher = "direct")]
+pub fn square_direct(i: &[f32], o: &mut [f32]) {
+    for (i, o) in i.iter().zip(o) {
+        *o = i * i;
+    }
+}
