a75fdae292dcc06a90a3c3da5b40edc379ffb5e2
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-08-16 15:16:10 -0400

    Fix broken test

--- multiversion/tests/destructure.rs
+++ multiversion/tests/destructure.rs
@@ -1,8 +1,6 @@
-#![allow(clippy::blacklisted_name)]
-
 struct Foo {
-    bar: i64,
-    baz: i64,
+    x: i64,
+    y: i64,
 }
 
 #[multiversion::multiversion(targets("x86_64+avx", "arm+neon"))]
@@ -11,8 +9,8 @@ fn destructure_tuple_multiversion((x, y): (i64, i64)) -> (i64, i64) {
 }
 
 #[multiversion::multiversion(targets("x86_64+avx", "arm+neon"))]
-fn destructure_struct_multiversion(Foo { bar, baz }: Foo) -> (i64, i64) {
-    (bar, baz)
+fn destructure_struct_multiversion(Foo { x, y }: Foo) -> (i64, i64) {
+    (x, y)
 }
 
 #[multiversion::multiversion(targets("x86_64+avx", "arm+neon"))]
@@ -21,8 +19,8 @@ fn destructure_tuple((x, y): (i64, i64)) -> (i64, i64) {
 }
 
 #[multiversion::multiversion(targets("x86_64+avx", "arm+neon"))]
-fn destructure_struct(Foo { bar, baz }: Foo) -> (i64, i64) {
-    (bar, baz)
+fn destructure_struct(Foo { x, y }: Foo) -> (i64, i64) {
+    (x, y)
 }
 
 #[multiversion::multiversion(targets("x86_64+avx", "arm+neon"))]
@@ -38,11 +36,8 @@ mod test {
     fn destructure() {
         assert_eq!(destructure_tuple((1, 2)), (1, 2));
         assert_eq!(destructure_tuple_multiversion((3, 4)), (3, 4));
-        assert_eq!(destructure_struct(Foo { bar: 1, baz: 2 }), (1, 2));
-        assert_eq!(
-            destructure_struct_multiversion(Foo { bar: 3, baz: 4 }),
-            (3, 4)
-        );
+        assert_eq!(destructure_struct(Foo { x: 1, y: 2 }), (1, 2));
+        assert_eq!(destructure_struct_multiversion(Foo { x: 3, y: 4 }), (3, 4));
         assert_eq!(destructure_tuple_generic((1i64, 2i64)), (1, 2));
     }
 }
