f71d536a4fd66c7190289bbf85cd5111ac1c6b57
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-12-04 21:03:04 -0500

    Fix no-std benchmarks

--- multiversion/benches/benchmark.rs
+++ multiversion/benches/benchmark.rs
@@ -1,6 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use multiversion::multiversion;
 
+#[cfg(feature = "std")]
 #[multiversion(targets = "simd", dispatcher = "indirect")]
 fn indirect_fn(values: &mut [f32]) {
     for v in values {
@@ -8,6 +9,7 @@ fn indirect_fn(values: &mut [f32]) {
     }
 }
 
+#[cfg(feature = "std")]
 #[multiversion(targets = "simd", dispatcher = "direct")]
 fn direct_fn(values: &mut [f32]) {
     for v in values {
@@ -30,19 +32,25 @@ fn base_fn(values: &mut [f32]) {
 
 pub fn benchmark_dispatcher(c: &mut Criterion) {
     // Don't profile initial feature detection
-    indirect_fn(&mut []);
-    direct_fn(&mut []);
+    #[cfg(feature = "std")]
+    {
+        indirect_fn(&mut []);
+        direct_fn(&mut []);
+    }
 
     for len in &[0, 16, 1000] {
         let mut g = c.benchmark_group(&format!("{len} elements"));
         let mut i = vec![0f32; *len];
+
+        #[cfg(feature = "std")]
         g.bench_function("indirect dispatcher", |b| {
             b.iter(|| indirect_fn(black_box(i.as_mut())))
         })
         .bench_function("direct dispatcher", |b| {
             b.iter(|| direct_fn(black_box(i.as_mut())))
-        })
-        .bench_function("static dispatcher", |b| {
+        });
+
+        g.bench_function("static dispatcher", |b| {
             b.iter(|| static_fn(black_box(i.as_mut())))
         })
         .bench_function("no multiversioning", |b| {
