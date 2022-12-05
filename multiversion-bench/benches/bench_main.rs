05294fab27c7aa005cf1ccbf5562e67cfdd017fc
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-12-04 19:42:04 -0500

    Update benchmarks

--- multiversion-bench/benches/bench_main.rs
+++
@@ -1,4 +0,0 @@
-use criterion::criterion_main;
-mod benchmarks;
-
-criterion_main!(benchmarks::square::bench_square);
---
+++ multiversion/benches/benchmark.rs
@@ -0,0 +1,52 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use multiversion::multiversion;
+
+#[multiversion(targets = "simd", dispatcher = "indirect")]
+fn indirect_fn(values: &mut [f32]) {
+    for v in values {
+        *v *= *v;
+    }
+}
+
+#[multiversion(targets = "simd", dispatcher = "direct")]
+fn direct_fn(values: &mut [f32]) {
+    for v in values {
+        *v *= *v;
+    }
+}
+
+#[multiversion(targets = "simd", dispatcher = "static")]
+fn static_fn(values: &mut [f32]) {
+    for v in values {
+        *v *= *v;
+    }
+}
+
+fn base_fn(values: &mut [f32]) {
+    for v in values {
+        *v *= *v;
+    }
+}
+
+pub fn benchmark_dispatcher(c: &mut Criterion) {
+    for len in &[0, 16, 1000] {
+        let mut g = c.benchmark_group(&format!("{len} elements"));
+        let mut i = vec![0f32; *len];
+        g.bench_function("indirect dispatcher", |b| {
+            b.iter(|| indirect_fn(black_box(i.as_mut())))
+        })
+        .bench_function("direct dispatcher", |b| {
+            b.iter(|| direct_fn(black_box(i.as_mut())))
+        })
+        .bench_function("static dispatcher", |b| {
+            b.iter(|| static_fn(black_box(i.as_mut())))
+        })
+        .bench_function("no multiversioning", |b| {
+            b.iter(|| base_fn(black_box(i.as_mut())))
+        });
+        g.finish();
+    }
+}
+
+criterion_group!(benches, benchmark_dispatcher);
+criterion_main!(benches);
