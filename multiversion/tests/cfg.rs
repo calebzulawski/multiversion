3d196e59609e57810dadc0042c45283ff149bc79
Caleb Zulawski <caleb.zulawski@gmail.com>
2026-08-14 17:44:34 -0400

    Consolidate tests

--- multiversion/tests/cfg.rs
+++ multiversion/tests/cfg.rs
@@ -1,3 +1,26 @@
+#![cfg_attr(
+    all(feature = "nightly", target_arch = "arm"),
+    feature(arm_target_feature, stdarch_arm_feature_detection)
+)]
+#![cfg_attr(
+    all(feature = "nightly", any(target_arch = "mips", target_arch = "mips64")),
+    feature(mips_target_feature, stdarch_mips_feature_detection)
+)]
+#![cfg_attr(
+    all(
+        feature = "nightly",
+        any(target_arch = "powerpc", target_arch = "powerpc64")
+    ),
+    feature(powerpc_target_feature, stdarch_powerpc_feature_detection)
+)]
+#![cfg_attr(
+    all(
+        feature = "nightly",
+        any(target_arch = "riscv32", target_arch = "riscv64")
+    ),
+    feature(riscv_target_feature, stdarch_riscv_feature_detection)
+)]
+
 #[cfg(feature = "target-features")]
 use multiversion::target::selected_target;
 use multiversion::{
